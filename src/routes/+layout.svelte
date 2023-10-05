68ff6fc58aaa8556b810d0bf2ed133389ef50f1f
Gustavo Basso <gubasso@eambar.net>
2023-10-05 11:57:39 -0300

    (up) Issue #migrate_from_jsdoc_to_typescript.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
-	import Header from '$lib/components/Header.svelte'
-	import Footer from '$lib/components/Footer.svelte'
-	import '$lib/styles/style.css'
+  import Header from '$lib/components/Header.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+  import '$lib/styles/style.css'
 </script>
 
 <Header />
 
 <main>
-	<slot />
+  <slot />
 </main>
 
 <Footer />
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-	/**
-	 * @typedef {Object} Data
-	 * @property {string} title
-	 * @property {string} date
-	 * @property {import('svelte').ComponentType} content
-	 */
+  /**
+   * @typedef {Object} Data
+   * @property {string} title
+   * @property {string} date
+   * @property {import('svelte').ComponentType} content
+   */
 
-	/** @type {Data} */
-	export let data
+  /** @type {Data} */
+  export let data
 </script>
 
 <article>
-	<h1>{data.title}</h1>
-	<p>Published: {data.date}</p>
-	<svelte:component this={data.content} />
+  <h1>{data.title}</h1>
+  <p>Published: {data.date}</p>
+  <svelte:component this={data.content} />
 </article>
