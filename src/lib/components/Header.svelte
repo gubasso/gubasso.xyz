24ce72990bf7f7448e041d1c79517992426c8779
Gustavo Basso <gubasso@eambar.net>
2023-10-19 14:31:40 -0300

    (up) Issue #landing_page_basic_layout.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,42 +1,74 @@
 <script>
   import logo from '$lib/images/g.png'
   let showmenu = false
-  let openmenu = false
+  let openedMenu = false
+  $: closedmenu = !openedMenu
 </script>
 
 <header>
-  <a href="/"><img src={logo} alt="g logo" /></a>
-  <div class="menu-btn">
-    <iconify-icon icon="material-symbols:menu" />
+  <div class="h-1">
+    <a href="/"><img src={logo} alt="g logo" /></a>
+    <button class="menu-btn" on:click={() => (openedMenu = !openedMenu)}>
+      <iconify-icon class:opened-menu-icon={closedmenu} icon="material-symbols:menu" />
+      <iconify-icon class:opened-menu-icon={openedMenu} icon="ph:x" />
+    </button>
   </div>
-  <nav>
+  <nav class:opened-menu={openedMenu}>
     <ul>
       <li>
-        <a href="/blog">Blog</a>
+        <a href="/blog" on:click={() => (openedMenu = false)}>Blog</a>
       </li>
       <li>
-        <a href="/projects">Projects</a>
+        <a href="/projects" on:click={() => (openedMenu = false)}>Projects</a>
       </li>
       <li>
-        <a rel="external" href="https://resume.gubasso.xyz">Resume</a>
+        <a rel="external" href="https://resume.gubasso.xyz" on:click={() => (openedMenu = false)}
+          >Resume</a
+        >
       </li>
     </ul>
   </nav>
+  <div class="top-menu" />
 </header>
 
 <style>
-  img {
-    width: var(--step-2);
-    object-fit: cover;
-    object-position: 50% 50%;
+  iconify-icon {
+    display: none;
   }
-  header {
-    position: relative;
+  .opened-menu-icon {
+    display: block;
+  }
+  .opened-menu {
+    margin: 0;
+    padding: 0;
+    left: 0px;
+    display: block;
+    position: absolute;
+    height: calc(100vh - 80px);
+    width: 100vw;
+    max-width: 100%;
+    background-color: var(--color-canvas-inset-transparent);
+  }
+  .opened-menu > ul {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .opened-menu a {
+    font-size: var(--step-3);
+    font-weight: bold;
+  }
+  .h-1 {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: var(--color-canvas-inset);
     padding: 1rem;
+  }
+  .top-menu {
+    position: relative;
+  }
+  header {
+    background-color: var(--color-canvas-inset);
   }
   nav {
     margin-top: 1rem;
@@ -47,14 +79,22 @@
     margin: 0;
     display: flex;
     gap: 1rem;
+    padding: 0;
   }
 
   a {
     text-decoration: none;
     color: inherit;
   }
+  img {
+    width: var(--step-2);
+    vertical-align: bottom;
+  }
 
   .menu-btn {
+    background: none;
+    border: none;
+    color: var(--color-fg-default);
     display: flex;
     font-size: var(--step-1);
     cursor: pointer;
@@ -69,9 +109,9 @@
   }
 
   @media screen and (min-width: 600px) {
-    nav {
-      margin-top: 0;
-      display: block;
-    }
+    /* nav { */
+    /*   margin-top: 0; */
+    /*   display: block; */
+    /* } */
   }
 </style>
